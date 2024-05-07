1b6eef928e5d761208f2d9c3f69512ff45aa0fd5
Louis Tsang <kawingt2@gmail.com>
2024-05-07 23:56:32 +0800

    fixed pathing

--- word_master/core.py
+++ word_master/core.py
@@ -1,7 +1,11 @@
+import pathlib
 from text_file_parser import TextFileParser
 
+
+DIR = pathlib.Path(__file__).parent.resolve()
+
 if __name__ == '__main__':
-    path = input("Enter a file path: ")
-    print(path)   
-    data = TextFileParser.parse_file(path)
+    filename = input("Enter a file name: ")  
+    parser = TextFileParser(str(DIR) + '/' + filename)
+    data = parser.parse_file()
     print(data)
