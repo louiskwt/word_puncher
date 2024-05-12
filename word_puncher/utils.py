5aea80e16ab2ba0ec7dcade66b801d262fbfa154
Louis Tsang <kawingt2@gmail.com>
2024-05-12 20:15:28 +0800

    wrote util functions to check common words and strip punctuations

--- word_puncher/utils.py
+++ word_puncher/utils.py
@@ -1,4 +1,11 @@
-import random
+import random, string
+
+COMMON_WORDS = {
+    'articles': ['a', 'an', 'the'],
+    'personal_pronouns': ['he', 'i', 'she', 'they', 'we', 'you', "he's", "i'm", "she's", "they're", "we're", "you're"],
+    'demonstrative_pronouns': ['that', 'these', 'this', 'those'],
+    'interrogative_pronouns': ['what', 'which', 'who', 'whom', 'whose']
+}
 
 def punch_out_words(lines):
     answer_keys = []
@@ -13,3 +20,37 @@ def punch_out_words(lines):
 
 def replace_word_with_blank(word):
     return "_" * len(word) * 2
+
+def strip_punctuations(word):
+    """
+    A function that strips punctuation at the beginning or end of a word
+    >>> word1 = "'Hey!'"
+    >>> strip_punctuations(word1)
+    'Hey'
+    >>> word2 = "This's?"
+    >>> strip_punctuations(word2)
+    "This's"
+    """
+    new_word = word
+    while new_word[0] in string.punctuation or new_word[-1] in string.punctuation:
+        if new_word[0] in string.punctuation:
+            new_word = new_word[1:]
+        if new_word[-1] in string.punctuation:
+            new_word = new_word[:-1]
+    return new_word
+
+
+def check_for_common_words(word):
+    """
+    A function that check whether a word is a common word in English
+    >>> a = "a"
+    >>> check_for_common_words(a)
+    True
+    >>> foo = "foo"
+    >>> check_for_common_words(foo)
+    False
+    >>> p = "He's"
+    >>> check_for_common_words(p)
+    True
+    """
+    return any([strip_punctuations(word.lower()) in COMMON_WORDS[key] for key in COMMON_WORDS])
