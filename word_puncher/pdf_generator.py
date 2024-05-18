dab8331fe21b71995d2870853867826f8767abed
Louis Tsang <kawingt2@gmail.com>
2024-05-18 17:55:49 +0800

    fixed heading and title style

--- word_puncher/pdf_generator.py
+++ word_puncher/pdf_generator.py
@@ -5,11 +5,9 @@ class PDF_Generator(FPDF):
         self.set_font("helvetica", "B", 14)
         width = self.get_string_width(self.title) + 6
         self.set_x((210 - width)/2)
-        self.set_draw_color(0, 80, 180)
-        self.set_fill_color(230, 230, 0)
-        self.set_text_color(220, 50, 50)
+        self.set_text_color(0, 50, 50)
         self.set_line_width(1)
-        self.cell(width, 9, self.title, border=1, new_x="LMARGIN", new_y="NEXT", align="C", fill=True)
+        self.cell(width, 9, self.title, border=0, new_x="LMARGIN", new_y="NEXT", align="C")
         self.ln(10)
     
     def footer(self):
@@ -19,8 +17,7 @@ class PDF_Generator(FPDF):
         
     def set_body_heading(self, heading):
         self.set_font("helvetica", "", 12)
-        self.set_fill_color(200, 220, 255)
-        self.cell(0, 6, heading, new_x="LMARGIN", new_y="NEXT", align="L", fill=True)
+        self.cell(0, 6, heading, new_x="LMARGIN", new_y="NEXT", align="L")
         self.ln(4)
 
     def print_page_content(self, heading, text):
