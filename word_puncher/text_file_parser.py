c62ad4d072c6c9d6df77228e6f08877288508728
Louis Tsang <kawingt2@gmail.com>
2024-05-21 23:58:55 +0800

    started working on docs parser

--- word_puncher/text_file_parser.py
+++ word_puncher/text_file_parser.py
@@ -1,11 +1,12 @@
 # Parser for files
-import os
-
 class TextFileParser:
     def __init__(self, path):
         self.path = path
     
-    def parse_file(self):
+    def parse_txt_file(self):
         with open(self.path) as file:
             return [line.rstrip('\n') for line in file if line.rstrip('\n')]
+    
+    def parse_docx_file(self):
+        pass
 
