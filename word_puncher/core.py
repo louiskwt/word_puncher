eddd6ec17d58ca82f1cbbbce0758fe88ea28ee4a
Louis Tsang <kawingt2@gmail.com>
2024-05-21 23:59:18 +0800

    updated parser name for test

--- word_puncher/core.py
+++ word_puncher/core.py
@@ -9,7 +9,7 @@ DIR = pathlib.Path(__file__).parent.resolve()
 if __name__ == '__main__':
     filename = input("Enter a file name: ") 
     parser = TextFileParser(str(DIR) + '/' + filename)
-    data = parser.parse_file()
+    data = parser.parse_txt_file()
     output, answers = punch_out_words(data, 1)
     for line in output:
         print(line)
