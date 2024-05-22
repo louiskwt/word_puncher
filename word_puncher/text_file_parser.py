dd8d772883391400ab7bd1246e5a10927e1948b6
Louis Tsang <kawingt2@gmail.com>
2024-05-22 08:25:48 +0800

    parsed docx with regex

--- word_puncher/text_file_parser.py
+++ word_puncher/text_file_parser.py
@@ -1,12 +1,21 @@
 # Parser for files
+import zipfile, re
+
 class TextFileParser:
     def __init__(self, path):
         self.path = path
+
+    def detect_file_type():
+        pass
     
     def parse_txt_file(self):
         with open(self.path) as file:
             return [line.rstrip('\n') for line in file if line.rstrip('\n')]
     
     def parse_docx_file(self):
-        pass
-
+        with zipfile.ZipFile(self.path, 'r') as file:
+            xml = file.read('word/document.xml')
+        xml_str = xml.decode('utf-8')
+        pattern = r'>(.*?)<'
+        text = [s for s in re.findall(pattern, xml_str) if s and s!='\r']
+        return text
