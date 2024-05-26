abbcacd1325b4c2418e79e21db6ecb1db0c76f76
Louis Tsang <kawingt2@gmail.com>
2024-05-26 10:48:03 +0800

    wrote function to detect text file

--- word_puncher/text_file_parser.py
+++ word_puncher/text_file_parser.py
@@ -4,9 +4,19 @@ import zipfile, re
 class TextFileParser:
     def __init__(self, path):
         self.path = path
+        self.parse = self.parse_txt_file 
 
-    def detect_file_type():
-        pass
+    def detect_file_type(self):
+        EXTENSION_SIGNATURE = {
+            "docx": "50 4B 03 04",
+        }
+        file = open(self.path, "rb").read(32)
+        file_hex_numbers = " ".join(['{:02X}'.format(byte) for byte in file])[:11]
+        print(file_hex_numbers)
+        for _, val in EXTENSION_SIGNATURE.items():
+            if file_hex_numbers == val:
+                self.parse = self.parse_docx_file
+        
     
     def parse_txt_file(self):
         with open(self.path) as file:
