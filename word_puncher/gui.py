15a715e34568f293c6522a6d7a26f07b5229c92c
Louis Tsang <kawingt2@gmail.com>
2024-05-28 23:25:20 +0800

    expanded gui

--- word_puncher/gui.py
+++ word_puncher/gui.py
@@ -1,4 +1,6 @@
-from tkinter import Label
-widget = Label(None, text='Hello! Word Puncher~')
-widget.pack()
-widget.mainloop()
+from tkinter import *
+root = Tk()
+widget = Label(root, text='Hello! Word Puncher~')
+widget.pack(side=TOP, expand=YES, fill=BOTH)
+root.title('Word Puncher')
+root.mainloop()
