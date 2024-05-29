074a249ad0f9e45bb57be9d15db75be06b274b7b
Louis Tsang <kawingt2@gmail.com>
2024-05-29 23:38:18 +0800

    resize window with geometry

--- word_puncher/gui.py
+++ word_puncher/gui.py
@@ -1,6 +1,9 @@
-from tkinter import *
-root = Tk()
-widget = Label(root, text='Hello! Word Puncher~')
-widget.pack(side=TOP, expand=YES, fill=BOTH)
+import tkinter as tk
+root = tk.Tk()
 root.title('Word Puncher')
+root.geometry('720x680+100+100')
+title = tk.Label(root, text="Word Puncher", font=('Arial 16 bold')).grid(row=2, columnspan=2)
+button = tk.Button(root, text="Add file").grid(row=2, column=1)
+word_inp = tk.Checkbutton(root, text="Check")
+
 root.mainloop()
