bcc982af6fd4077009e8305c4bec4dfe40dabec6
Louis Tsang <kawingt2@gmail.com>
2024-06-02 13:02:03 +0800

    changed direction again for gui

--- word_puncher/gui.py
+++ word_puncher/gui.py
@@ -1,7 +1,19 @@
-from flask import Flask
+import http.server
+import cgi
 
-app = Flask(__name__)
+# TODO: learn to roll a small local server for the gui
+class HttpHandler(http.server.SimpleHTTPRequestHandler):
+    #Override the default do_POST method
+    def log_message(self, format, *args):
+        pass 
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello! Word Puncher</h1>"
+    def cgiFieldStorageToDict(self, fieldStorage):
+        """ Get a plain dictionary rather than the '.value' system used by the
+           cgi module's native fieldStorage class. """
+        params = {}
+        for key in fieldStorage.keys():
+            params[key] = fieldStorage[key].value
+        return params
+
+    def do_POST(self):
+        pass
