e1e191b3c145d5407dfd0b869a175faaabbcb42f
Louis Tsang <kawingt2@gmail.com>
2024-06-03 23:52:52 +0800

    handle get request in gui

--- word_puncher/gui.py
+++ word_puncher/gui.py
@@ -1,19 +1,32 @@
-import http.server
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 import cgi
 
 # TODO: learn to roll a small local server for the gui
-class HttpHandler(http.server.SimpleHTTPRequestHandler):
+class HttpHandler(SimpleHTTPRequestHandler):
     #Override the default do_POST method
     def log_message(self, format, *args):
         pass 
 
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'gui.html'
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+
     def cgiFieldStorageToDict(self, fieldStorage):
         """ Get a plain dictionary rather than the '.value' system used by the
            cgi module's native fieldStorage class. """
-        params = {}
-        for key in fieldStorage.keys():
-            params[key] = fieldStorage[key].value
-        return params
+        pass
 
     def do_POST(self):
         pass
+
+
+def run_gui(port):
+    httpd = HTTPServer(('localhost', port), HttpHandler)
+    print(f"Running Word Puncher on localhost port: {port}...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run_gui(8000)
