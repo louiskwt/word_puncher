b019d44dbb8d4e56a043bd94688d56ba3acfc925
Louis Tsang <kawingt2@gmail.com>
2024-06-05 00:16:43 +0800

    wip GUI controller logic

--- word_puncher/gui.py
+++ word_puncher/gui.py
@@ -1,5 +1,33 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from utils import generate_exercise_with_answer_keys
 import cgi
+import re
+
+class GUI:
+    '''
+        the main logic for the web based gui
+        it acts like a controllers
+    '''
+    def __init__(self):
+        self.selected_words = []
+        self.selected_text = []
+
+    def parse_uploaded_file(self, text_data, is_word):
+        if is_word:
+            pattern = r'>(.*?)<'
+            text = [s for s in re.findall(pattern, text_data) if s and s!='\r']
+        else:
+            text = [line.rstrip('\n') for line in text_data if line.rstrip('\n')]
+
+        self.selected_text = text
+        return [[word for word in line] for line in text] 
+
+    def parse_selected_words(self, text_data):
+        self.selected_words = text_data
+    
+    def generate_exercise(self):
+        return generate_exercise_with_answer_keys(self.selected_text, self.selected_words)
+
 
 # TODO: learn to roll a small local server for the gui
 class HttpHandler(SimpleHTTPRequestHandler):
