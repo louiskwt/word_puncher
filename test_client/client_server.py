52b0a4118a40d18f9fbd8cc5c1345aaa649259f6
Louis Tsang <kawingt2@gmail.com>
2024-06-09 23:44:14 +0800

    parsed data from rquest and tested

--- test_client/client_server.py
+++ test_client/client_server.py
@@ -1,4 +1,5 @@
-from bottle import route, post, run, template
+from bottle import route, post, run, template, request
+from text_file_parser import TextFileParser
 
 @route("/")
 def index():
@@ -6,6 +7,16 @@ def index():
 
 @post('/upload-file')
 def upload_file():
-    return template('client', output="File output here")
+    file = request.forms.get('text-file')
+    if file:
+        print("here", request.forms.get('text-file'))
+        parser = TextFileParser(file)
+        parser.detect_file_type()
+        data = parser.parse()
+        print(data)
+        output_data = ['<div>' + line + '</div>' for line in data]
+        return template('client', output=''.join(output_data))
+    else:
+        return template('client', output="File output here")
 
 run(host='localhost', port=8000, debug=True, reloader=True)
