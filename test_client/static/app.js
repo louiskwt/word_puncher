75441abc3fb4d4723e5382b9904c5cd8a411ea4e
Louis Tsang <kawingt2@gmail.com>
2024-06-11 23:43:10 +0800

    enable select word mchanism in js

--- test_client/static/app.js
+++ test_client/static/app.js
@@ -4,4 +4,9 @@ const outPutDiv = document.getElementById("output");
 
 outPutDiv.addEventListener("dblclick", function handleClick(e) {
   console.log(e.target);
+  if (e.target.tagName === "SPAN") {
+    console.log("span");
+    e.target.style.backgroundColor = "yellow";
+    e.target.className = "highlighted";
+  }
 });
