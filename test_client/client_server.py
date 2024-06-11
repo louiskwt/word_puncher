5d0de5fcc68fc43e93ef783d44e6caebc1a911be
Louis Tsang <kawingt2@gmail.com>
2024-06-11 23:21:08 +0800

    reformated output with util

--- test_client/client_server.py
+++ test_client/client_server.py
@@ -1,5 +1,6 @@
 from bottle import route, post, run, template, request
 from text_file_parser import TextFileParser
+from utils import strip_punctuations, find_punctuation
 
 @route("/")
 def index():
@@ -9,12 +10,19 @@ def index():
 def upload_file():
     file = request.forms.get('text-file')
     if file:
-        print("here", request.forms.get('text-file'))
         parser = TextFileParser(file)
         parser.detect_file_type()
         data = parser.parse()
-        print(data)
-        output_data = ['<div>' + line + '</div>' for line in data]
+        output_data, id_count = [], 0
+        for line in data:
+            words = line.split(" ") 
+            processed_words = []
+            for word in words:
+                punctuations, cleaned_word = find_punctuation(word), strip_punctuations(word)
+                processed_words.append('<span id="' + str(id_count) + '">' + punctuations['front'] + cleaned_word + punctuations['end'] + '</span>')
+                id_count += 1
+
+            output_data.append("<div>" +  " ".join(processed_words) + "</div>")
         return template('client', output=''.join(output_data))
     else:
         return template('client', output="File output here")
